dd75613b9a67fdfce45c369360d430cf2ae4ea9b
schlouff <33584736+schlouff@users.noreply.github.com>
2024-06-16 21:20:31 +0200

    nochmals Downgrade auf Image Description

--- ChatCloneStreamlit.py
+++ ChatCloneStreamlit.py
@@ -1,4 +1,4 @@
-#Scham mit Image_Description
+#Scham mit Image description
 import streamlit as st
 import os
 import time
@@ -69,7 +69,7 @@ def create_artistic_description(responses):
 if __name__ == '__main__':
     col1, col2 = st.columns([0.85, 0.15])
     with col1:
-        st.title('Scham mit Image_Description')
+        st.title('Image Description')
     with col2:
         st.image('ai.png', width=70)
 
